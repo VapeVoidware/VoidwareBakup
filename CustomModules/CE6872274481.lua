5b375090345e33df31570346d236773130910e71
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-10-28 06:38:38 +0200

    Upd

--- CustomModules/CE6872274481.lua
+++ CustomModules/CE6872274481.lua
@@ -5293,16 +5293,16 @@ run(function()
 									killaurarealremote:FireServer({
 										weapon = sword.tool,
 										chargedAttack = {chargeRatio = 0},
+										lastSwingServerTimeDelta = 10,
 										entityInstance = plr.Character,
 										validate = {
 											raycast = {
-												cameraPosition = attackValue(root.Position),
-												cursorDirection = attackValue(CFrame.new(selfpos, root.Position).lookVector)
+												cameraPosition = {value = vector.create(0/0,0/0,0/0)},
+												cursorDirection = {value = vector.create(0/0,0/0,0/0)}
 											},
-											targetPosition = attackValue(root.Position),
-											selfPosition = attackValue(selfpos)
-										},
-										--lastSwingServerTimeDelta = lastSwingServerTimeDelta
+											targetPosition = {value = vector.create(0/0,0/0,0/0)},
+											selfPosition = {value = vector.create(0/0,0/0,0/0)}
+										}
 									})
 									local spear = getItemNear('spear')
 									if spear then
