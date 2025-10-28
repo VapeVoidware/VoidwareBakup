11137b55af2a9dcbed20fe9c97d5ee22bba100fb
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-10-28 06:39:20 +0200

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -594,7 +594,7 @@ local url = shared.RiseMode and "https://github.com/VapeVoidware/VWRise/" or "ht
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
 shared.RiseMode = false
-commit = "b7f2d2f0165b9a328e087ebcc0a8f701c7ba1152"
+commit = "5b375090345e33df31570346d236773130910e71"
 --[[if not shared.RiseMode then
     commit = "60143661ed276fb2600b1a5f09015ecce12ead82"
 else
